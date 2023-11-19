c459d63534bc2c77234979915da6896e767b1c17
ypjun100 <ypjun100@gmail.com>
2023-11-19 17:05:53 +0900

    ADD: Lawy_Summarization 프로젝트 생성

---
+++ web/Lawy_Summarization/src/main/java/com/lawy/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.lawy;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(LawySummarizationApplication.class);
+	}
+
+}
---
+++ web/Lawy_Summarization/src/test/java/com/lawy/LawySummarizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lawy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LawySummarizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
